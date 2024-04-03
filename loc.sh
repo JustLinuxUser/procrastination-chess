696d2ab67103835145346b8819c4ec24c7ff5190
Andrii Dokhniak <andokhniak@gmail.com>
2024-04-03 20:28:27 +0200

    fixup the repo

--- loc.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/bash
-#
-OLD=1464
-LOC=$(cloc . | grep Java | grep -v JavaScript| awk -F" " '{print $5}')
-echo $(($LOC - $OLD))
--- script.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -XX:+PreserveFramePointer -cp /home/andriy/Programming/langs/cava/chess_from_scratch/app/build/libs/app.jar org.example.App "$1" "$2" "$3"
